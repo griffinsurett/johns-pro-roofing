5a50ed6f7f6f63780f7d01be23194d2a05152d7e
griffinjoshs <leveloutusa@gmail.com>
2026-07-13 16:47:52 -0400

    roofs

--- src/components/Menu/HamburgerMenuDrawer.tsx
+++ src/components/Menu/HamburgerMenuDrawer.tsx
@@ -96,7 +96,7 @@ export default function MobileMenuDrawer({
       />
 
       <div
-        className={`fixed left-0 right-0 z-50 w-screen transform transition-all duration-200 ${
+        className={`fixed left-0 right-0 z-50 transform transition-all duration-200 ${
           isOpen
             ? "pointer-events-auto translate-y-0 opacity-100"
             : "pointer-events-none -translate-y-2 opacity-0"
